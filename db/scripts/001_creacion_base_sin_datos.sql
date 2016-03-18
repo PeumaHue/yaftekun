cc273d0dea7fc2b2bb3b329c8c4965862c771fad
marianocat <mariano.andolfatto@gmail.com>
2016-03-18 17:20:03 -0300

    Imagenes de equipos para usar; reestructuracion de carpeta de scripts;

--- db/scripts/001_creacion_base_sin_datos.sql
+++ db/scripts/001_creacion_base_sin_datos.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema tepaundacampeonato
+-- Schema yaftekun
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema tepaundacampeonato
+-- Schema yaftekun
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `tepaundacampeonato` DEFAULT CHARACTER SET utf8 ;
-USE `tepaundacampeonato` ;
+CREATE SCHEMA IF NOT EXISTS `yaftekun` DEFAULT CHARACTER SET utf8 ;
+USE `yaftekun` ;
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`grupo`
+-- Table `yaftekun`.`grupo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`grupo` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`grupo` (
   `idgrupo` INT(10) UNSIGNED NOT NULL,
   `nombre` VARCHAR(50) NULL DEFAULT NULL,
   PRIMARY KEY (`idgrupo`))
@@ -26,9 +26,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`tiporonda`
+-- Table `yaftekun`.`tiporonda`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`tiporonda` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`tiporonda` (
   `idtiporonda` INT(10) UNSIGNED NOT NULL,
   `nombre` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`idtiporonda`))
@@ -37,9 +37,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`grupo_por_ronda`
+-- Table `yaftekun`.`grupo_por_ronda`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`grupo_por_ronda` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`grupo_por_ronda` (
   `id_grupo_por_ronda` BIGINT(20) UNSIGNED NOT NULL,
   `idtorneo` BIGINT(20) UNSIGNED NOT NULL,
   `idronda` BIGINT(20) UNSIGNED NOT NULL,
@@ -50,26 +50,26 @@ CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`grupo_por_ronda` (
   PRIMARY KEY (`id_grupo_por_ronda`),
   CONSTRAINT `fk_grupo_por_ronda_grupo`
     FOREIGN KEY (`idgrupo`)
-    REFERENCES `tepaundacampeonato`.`grupo` (`idgrupo`)
+    REFERENCES `yaftekun`.`grupo` (`idgrupo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_grupo_por_ronda_tiporonda`
     FOREIGN KEY (`idtiporonda`)
-    REFERENCES `tepaundacampeonato`.`tiporonda` (`idtiporonda`)
+    REFERENCES `yaftekun`.`tiporonda` (`idtiporonda`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE INDEX `fk_grupo_por_ronda_grupo_idx` ON `tepaundacampeonato`.`grupo_por_ronda` (`idgrupo` ASC);
+CREATE INDEX `fk_grupo_por_ronda_grupo_idx` ON `yaftekun`.`grupo_por_ronda` (`idgrupo` ASC);
 
-CREATE INDEX `fk_grupo_por_ronda_tiporonda_idx` ON `tepaundacampeonato`.`grupo_por_ronda` (`idtiporonda` ASC);
+CREATE INDEX `fk_grupo_por_ronda_tiporonda_idx` ON `yaftekun`.`grupo_por_ronda` (`idtiporonda` ASC);
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`asignacion_en_grupo_por_ronda`
+-- Table `yaftekun`.`asignacion_en_grupo_por_ronda`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`asignacion_en_grupo_por_ronda` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`asignacion_en_grupo_por_ronda` (
   `asignacion_en_grupo_por_ronda` BIGINT(20) UNSIGNED NOT NULL,
   `id_grupo_por_ronda` BIGINT(20) UNSIGNED NOT NULL,
   `puesto_equipo_a` INT(10) UNSIGNED NOT NULL,
@@ -77,19 +77,19 @@ CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`asignacion_en_grupo_por_ronda` 
   PRIMARY KEY (`asignacion_en_grupo_por_ronda`),
   CONSTRAINT `fk_asignacion_en_grupo_por_ronda_grupo_por_ronda`
     FOREIGN KEY (`id_grupo_por_ronda`)
-    REFERENCES `tepaundacampeonato`.`grupo_por_ronda` (`id_grupo_por_ronda`)
+    REFERENCES `yaftekun`.`grupo_por_ronda` (`id_grupo_por_ronda`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE INDEX `fk_asignacion_en_grupo_por_ronda_grupo_por_ronda_idx` ON `tepaundacampeonato`.`asignacion_en_grupo_por_ronda` (`id_grupo_por_ronda` ASC);
+CREATE INDEX `fk_asignacion_en_grupo_por_ronda_grupo_por_ronda_idx` ON `yaftekun`.`asignacion_en_grupo_por_ronda` (`id_grupo_por_ronda` ASC);
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`equipo`
+-- Table `yaftekun`.`equipo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`equipo` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`equipo` (
   `idequipo` BIGINT(20) UNSIGNED NOT NULL,
   `nombre` VARCHAR(150) NOT NULL,
   `imagen` MEDIUMBLOB NULL DEFAULT NULL,
@@ -99,9 +99,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`sede`
+-- Table `yaftekun`.`sede`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`sede` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`sede` (
   `idsede` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`idsede`))
@@ -110,9 +110,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`fixture`
+-- Table `yaftekun`.`fixture`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`fixture` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`fixture` (
   `idencuentro` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `idtorneo` BIGINT(20) UNSIGNED NOT NULL,
   `idronda` INT(10) UNSIGNED NOT NULL,
@@ -126,37 +126,37 @@ CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`fixture` (
   PRIMARY KEY (`idencuentro`),
   CONSTRAINT `fk_fixture_equipoa`
     FOREIGN KEY (`idequipoa`)
-    REFERENCES `tepaundacampeonato`.`equipo` (`idequipo`)
+    REFERENCES `yaftekun`.`equipo` (`idequipo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_fixture_equipob`
     FOREIGN KEY (`idequipob`)
-    REFERENCES `tepaundacampeonato`.`equipo` (`idequipo`)
+    REFERENCES `yaftekun`.`equipo` (`idequipo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_fixture_sede`
     FOREIGN KEY (`idsede`)
-    REFERENCES `tepaundacampeonato`.`sede` (`idsede`)
+    REFERENCES `yaftekun`.`sede` (`idsede`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE UNIQUE INDEX `idencuentro_UNIQUE` ON `tepaundacampeonato`.`fixture` (`idencuentro` ASC);
+CREATE UNIQUE INDEX `idencuentro_UNIQUE` ON `yaftekun`.`fixture` (`idencuentro` ASC);
 
-CREATE INDEX `idequipoa` ON `tepaundacampeonato`.`fixture` (`idequipoa` ASC);
+CREATE INDEX `idequipoa` ON `yaftekun`.`fixture` (`idequipoa` ASC);
 
-CREATE INDEX `idequipob` ON `tepaundacampeonato`.`fixture` (`idequipob` ASC);
+CREATE INDEX `idequipob` ON `yaftekun`.`fixture` (`idequipob` ASC);
 
-CREATE INDEX `idtorneo` ON `tepaundacampeonato`.`fixture` (`idtorneo` ASC);
+CREATE INDEX `idtorneo` ON `yaftekun`.`fixture` (`idtorneo` ASC);
 
-CREATE INDEX `fk_fixture_sede_idx` ON `tepaundacampeonato`.`fixture` (`idsede` ASC);
+CREATE INDEX `fk_fixture_sede_idx` ON `yaftekun`.`fixture` (`idsede` ASC);
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`participante`
+-- Table `yaftekun`.`participante`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`participante` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`participante` (
   `idparticipante` BIGINT(20) UNSIGNED NOT NULL,
   `nombre` VARCHAR(45) NULL,
   PRIMARY KEY (`idparticipante`))
@@ -164,9 +164,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`tipo_participante`
+-- Table `yaftekun`.`tipo_participante`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`tipo_participante` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`tipo_participante` (
   `idtipodeparticipante` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(50) NULL DEFAULT NULL,
   `descripcion` VARCHAR(200) NULL DEFAULT NULL,
@@ -176,9 +176,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`encuentro_participante`
+-- Table `yaftekun`.`encuentro_participante`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`encuentro_participante` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`encuentro_participante` (
   `idparticipante` BIGINT(20) UNSIGNED NOT NULL,
   `idencuentro` BIGINT(20) UNSIGNED NOT NULL,
   `idtipodeparticipante` INT(11) UNSIGNED NOT NULL,
@@ -187,35 +187,35 @@ CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`encuentro_participante` (
   PRIMARY KEY (`idparticipante`, `idencuentro`),
   CONSTRAINT `fk_encuentro_participante_fixture`
     FOREIGN KEY (`idencuentro`)
-    REFERENCES `tepaundacampeonato`.`fixture` (`idencuentro`)
+    REFERENCES `yaftekun`.`fixture` (`idencuentro`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_encuentro_participante_participante`
     FOREIGN KEY (`idparticipante`)
-    REFERENCES `tepaundacampeonato`.`participante` (`idparticipante`)
+    REFERENCES `yaftekun`.`participante` (`idparticipante`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_encuentro_participante_tipo_tarticipante`
     FOREIGN KEY (`idtipodeparticipante`)
-    REFERENCES `tepaundacampeonato`.`tipo_participante` (`idtipodeparticipante`)
+    REFERENCES `yaftekun`.`tipo_participante` (`idtipodeparticipante`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE INDEX `idequipo` ON `tepaundacampeonato`.`encuentro_participante` (`idequipo` ASC);
+CREATE INDEX `idequipo` ON `yaftekun`.`encuentro_participante` (`idequipo` ASC);
 
-CREATE INDEX `idparticipante` ON `tepaundacampeonato`.`encuentro_participante` (`idparticipante` ASC);
+CREATE INDEX `idparticipante` ON `yaftekun`.`encuentro_participante` (`idparticipante` ASC);
 
-CREATE INDEX `fk_encuentro_participante_fixture_idx` ON `tepaundacampeonato`.`encuentro_participante` (`idencuentro` ASC);
+CREATE INDEX `fk_encuentro_participante_fixture_idx` ON `yaftekun`.`encuentro_participante` (`idencuentro` ASC);
 
-CREATE INDEX `fk_encuentro_participante_tipo_tarticipante_idx` ON `tepaundacampeonato`.`encuentro_participante` (`idtipodeparticipante` ASC);
+CREATE INDEX `fk_encuentro_participante_tipo_tarticipante_idx` ON `yaftekun`.`encuentro_participante` (`idtipodeparticipante` ASC);
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`tipo_evento`
+-- Table `yaftekun`.`tipo_evento`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`tipo_evento` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`tipo_evento` (
   `idtipo_evento` INT(11) UNSIGNED NOT NULL,
   `nombre` VARCHAR(50) NULL DEFAULT NULL,
   PRIMARY KEY (`idtipo_evento`))
@@ -224,9 +224,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`evento_por_encuentro`
+-- Table `yaftekun`.`evento_por_encuentro`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`evento_por_encuentro` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`evento_por_encuentro` (
   `idevento_por_encuentro` BIGINT(20) UNSIGNED NOT NULL,
   `idtorneo` BIGINT(20) UNSIGNED NOT NULL,
   `idtipo_evento` INT(11) UNSIGNED NOT NULL,
@@ -236,33 +236,33 @@ CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`evento_por_encuentro` (
   PRIMARY KEY (`idevento_por_encuentro`),
   CONSTRAINT `fk_eventos_por_encuentro_fixture`
     FOREIGN KEY (`idencuentro`)
-    REFERENCES `tepaundacampeonato`.`fixture` (`idencuentro`)
+    REFERENCES `yaftekun`.`fixture` (`idencuentro`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_eventos_por_encuentro_tipo_evento`
     FOREIGN KEY (`idtipo_evento`)
-    REFERENCES `tepaundacampeonato`.`tipo_evento` (`idtipo_evento`)
+    REFERENCES `yaftekun`.`tipo_evento` (`idtipo_evento`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE INDEX `fk_eventos_por_encuentro_fixture_idx` ON `tepaundacampeonato`.`evento_por_encuentro` (`idencuentro` ASC);
+CREATE INDEX `fk_eventos_por_encuentro_fixture_idx` ON `yaftekun`.`evento_por_encuentro` (`idencuentro` ASC);
 
-CREATE INDEX `fk_eventos_por_encuentro_tipo_evento_idx` ON `tepaundacampeonato`.`evento_por_encuentro` (`idtipo_evento` ASC);
+CREATE INDEX `fk_eventos_por_encuentro_tipo_evento_idx` ON `yaftekun`.`evento_por_encuentro` (`idtipo_evento` ASC);
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`sancion`
+-- Table `yaftekun`.`sancion`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`sancion` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`sancion` (
   `idevento_por_encuentro` BIGINT(20) UNSIGNED NOT NULL,
   `cantidad_fecha_aplicada` INT(10) UNSIGNED NULL DEFAULT NULL,
   `cantidad_fecha_cumplida` INT(10) UNSIGNED NULL DEFAULT NULL,
   PRIMARY KEY (`idevento_por_encuentro`),
   CONSTRAINT `fk_sancione_evento_por_encuentro`
     FOREIGN KEY (`idevento_por_encuentro`)
-    REFERENCES `tepaundacampeonato`.`evento_por_encuentro` (`idevento_por_encuentro`)
+    REFERENCES `yaftekun`.`evento_por_encuentro` (`idevento_por_encuentro`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -270,9 +270,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`tabla_posiciones`
+-- Table `yaftekun`.`tabla_posiciones`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`tabla_posiciones` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`tabla_posiciones` (
   `idtorneo` BIGINT(20) UNSIGNED NOT NULL,
   `idequipo` BIGINT(20) UNSIGNED NOT NULL,
   `posicion` INT(10) UNSIGNED NULL DEFAULT NULL,
@@ -282,9 +282,9 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`tipodetorneo`
+-- Table `yaftekun`.`tipodetorneo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`tipodetorneo` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`tipodetorneo` (
   `idtipodetorneo` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(50) NOT NULL,
   `descripcion` VARCHAR(200) NULL DEFAULT NULL,
@@ -294,74 +294,74 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`torneo`
+-- Table `yaftekun`.`torneo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`torneo` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`torneo` (
   `idtorneo` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(200) NOT NULL,
   `idtipodetorneo` INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`idtorneo`),
   CONSTRAINT `fk_torneo_tipodetorneo`
     FOREIGN KEY (`idtipodetorneo`)
-    REFERENCES `tepaundacampeonato`.`tipodetorneo` (`idtipodetorneo`)
+    REFERENCES `yaftekun`.`tipodetorneo` (`idtipodetorneo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE INDEX `idtipodetorneo` ON `tepaundacampeonato`.`torneo` (`idtipodetorneo` ASC);
+CREATE INDEX `idtipodetorneo` ON `yaftekun`.`torneo` (`idtipodetorneo` ASC);
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`equipo_integrantes`
+-- Table `yaftekun`.`equipo_integrantes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`equipo_integrantes` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`equipo_integrantes` (
   `idequipo` BIGINT(20) UNSIGNED NOT NULL,
   `idparticipante` BIGINT(20) UNSIGNED NOT NULL,
   `idtipoparticipante` INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`idequipo`, `idparticipante`),
   CONSTRAINT `fk_equipo_integrantes_equipo`
     FOREIGN KEY (`idequipo`)
-    REFERENCES `tepaundacampeonato`.`equipo` (`idequipo`)
+    REFERENCES `yaftekun`.`equipo` (`idequipo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_equipo_integrantes_participante`
     FOREIGN KEY (`idparticipante`)
-    REFERENCES `tepaundacampeonato`.`participante` (`idparticipante`)
+    REFERENCES `yaftekun`.`participante` (`idparticipante`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_equipo_integrantes_tipo_participante`
     FOREIGN KEY (`idtipoparticipante`)
-    REFERENCES `tepaundacampeonato`.`tipo_participante` (`idtipodeparticipante`)
+    REFERENCES `yaftekun`.`tipo_participante` (`idtipodeparticipante`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_equipo_integrantes_participante_idx` ON `tepaundacampeonato`.`equipo_integrantes` (`idparticipante` ASC);
+CREATE INDEX `fk_equipo_integrantes_participante_idx` ON `yaftekun`.`equipo_integrantes` (`idparticipante` ASC);
 
-CREATE INDEX `fk_equipo_integrantes_tipo_participante_idx` ON `tepaundacampeonato`.`equipo_integrantes` (`idtipoparticipante` ASC);
+CREATE INDEX `fk_equipo_integrantes_tipo_participante_idx` ON `yaftekun`.`equipo_integrantes` (`idtipoparticipante` ASC);
 
 
 -- -----------------------------------------------------
--- Table `tepaundacampeonato`.`quipo_por_torneo`
+-- Table `yaftekun`.`quipo_por_torneo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tepaundacampeonato`.`quipo_por_torneo` (
+CREATE TABLE IF NOT EXISTS `yaftekun`.`quipo_por_torneo` (
   `idtorneo` BIGINT(20) UNSIGNED NOT NULL,
   `idequipo` BIGINT(20) UNSIGNED NOT NULL,
   PRIMARY KEY (`idtorneo`, `idequipo`),
   CONSTRAINT `fk_quipo_por_torneo_torneo`
     FOREIGN KEY (`idtorneo`)
-    REFERENCES `tepaundacampeonato`.`torneo` (`idtorneo`)
+    REFERENCES `yaftekun`.`torneo` (`idtorneo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_quipo_por_torneo_equipo`
     FOREIGN KEY (`idequipo`)
-    REFERENCES `tepaundacampeonato`.`equipo` (`idequipo`)
+    REFERENCES `yaftekun`.`equipo` (`idequipo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_quipo_por_torneo_equipo_idx` ON `tepaundacampeonato`.`quipo_por_torneo` (`idequipo` ASC);
+CREATE INDEX `fk_quipo_por_torneo_equipo_idx` ON `yaftekun`.`quipo_por_torneo` (`idequipo` ASC);
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
