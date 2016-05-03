b80bd3f7c59382d40f6ee62c5ec2657306330c49
Sergio <sergio_raimundo@hotmail.com>
2016-05-03 10:32:13 -0300

    se crea el sitio de html

---
+++ doc/pagina_modelo/styles/js/valida_equipos.js
@@ -0,0 +1,79 @@
+﻿
+$(document).ready(function () {
+    $('#equipos').bootstrapValidator({
+        message: 'Este valor no es valido',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            nombre: {
+                validators: {
+                    notEmpty: {
+                        message: 'El nombre de equipo es obligatorio'
+                    }
+                }
+            },
+            calle: {
+                validators: {
+                    notEmpty: {
+                        message: 'El nombre de la calle es obligatoria'
+                    }
+                }
+            },
+            numero: {
+                validators: {
+                    notEmpty: {
+                        message: 'La altura es obligatoria'
+                    },
+                    regexp: {
+                        regexp: /^[0-9]*$/,
+                        message: "El valor debe ser numérico"
+                    }
+
+                }
+            },
+            codigopostal: {
+                validators: {
+                    regexp: {
+                        regexp: /(^[0-9]{4}$)|(^[A-Za-z]{1}[0-9]{4}[A-Za-z]{3}$)/,
+                        message: 'Código postal no valido'
+                    }
+                }
+            },
+
+            provincia: {
+                validators: {
+                    notEmpty: {
+                        message: 'Debe seleccionar una provincia'
+                    }
+                }
+            },
+            localidad: {
+                validators: {
+                    notEmpty: {
+                        message: 'Debe seleccionar una localidad'
+                    }
+                }
+            },
+            telefono: {
+                validators: {
+                    digits: {
+                        message: 'solo se admiten números'
+                    },
+                    notEmpty: {
+                        message: 'debe ingresar un teléfono'
+                    }
+                }
+            },
+            mail: {
+                validators: {
+                    emailAddress: {
+                        messge: 'mail incorrecto'
+                    }
+                }
+            }
+        }
+    });
+});
---
+++ doc/pagina_modelo/styles/js/valida_torneo.js
@@ -0,0 +1,49 @@
+﻿
+$(document).ready(function () {
+    $('#torneo').bootstrapValidator({
+        message: 'Este valor no es valido',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            nombre_torneo: {
+                validators: {
+                    notEmpty: {
+                        message: 'Debe ingresar un nombre de torneo'
+                    }
+                }
+            },
+            liga: {
+                validators: {
+                    notEmpty: {
+                        message: 'Debe seleccionar una liga'
+                    }
+                }
+            },
+            anio: {
+                validators: {
+                    notEmpty: {
+                        message: 'Ingrese un año'
+                    },
+                    regexp: {
+                        regexp: /^20[1-9]{2}|2[0-9]{3}$/,
+                        message: "El año ingrsado no es valido"
+                    }
+
+                }
+            },
+            cantidad_equipos: {
+                validators: {
+                    digits: {
+                        message: 'solo se admiten números'
+                    },
+                    notEmpty: {
+                        message: 'debe ingresar la cantidad de equipos'
+                    }
+                }
+            }
+        }
+    });
+});
