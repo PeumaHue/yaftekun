52ff4497b034bcfbf4f078d6620f9a42ecf4b963
marianocat <mariano.andolfatto@gmail.com>
2016-05-06 16:48:04 -0300

    modelo y test de equipo

---
+++ app/application/controllers/test/Equipo_test.php
@@ -0,0 +1,161 @@
+<?php
+class Equipo_test extends CI_Controller {
+	
+	/**
+	 * Equipo_prueba se usa para poder probar completo ABM de equipos.
+	 * 
+	 */
+	public $id_equipo;
+	public $id_liga;
+	public $nombre;
+	public $id_estadio;
+	public $id_usuario;
+	public $imagen;
+	
+	public $iniciado;
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Equipo_model');
+		$this->load->library('unit_test');
+	}
+	
+	/**
+	 * Funcion que se carga por default al invocar al controlador sin especificar la URL completa
+	 * @return void
+	 */
+	public function index()
+	{
+		
+		$this->iniciado=TRUE;
+		
+		$this->alta_test();
+		mysqli_next_result($this->db->conn_id);
+				
+		$this->consulta_equipo_puntual_test();
+		mysqli_next_result($this->db->conn_id);
+		
+		$this->consulta_equipo_liga_test();
+		mysqli_next_result($this->db->conn_id);
+		
+		$this->consulta_equipo_inexistente_test();
+		mysqli_next_result($this->db->conn_id);
+		
+		$this->baja_test($this->id_equipo);
+		#mysqli_next_result($this->db->conn_id);
+
+		echo $this->unit->report();
+		
+		echo '<div>otras funciones para testear:<br>
+				<ul>
+					<li>/baja_test/nro-equipo</li>
+				</ul></div>';
+	}
+	
+	/**
+	 * Funcion para testear el alta satisfactoria de una persona
+	 * @return void
+	 */
+	public function alta_test()
+	{
+		$equipo = new stdClass();
+		
+		$equipo->id_liga=1;
+		$equipo->nombre='aldosivi';
+		$equipo->id_estadio=1;
+		$equipo->id_usuario=1;
+		$equipo->fecha_creacion='20160101';
+		$equipo->imagen='C:\Bitnami\wampstack-5.6.19-0\apps\yaftekun\db\imagenes\aldosivi.png';
+		
+		
+		$test = $this->Equipo_model->alta($equipo);
+		
+		$this->id_equipo = $test['id'];			
+		$expected_result['resultado']='OK';
+		$expected_result['id']=$test['id'];
+		$test_name = 'Alta equipo';
+		$notes = var_export($test, true);
+		$this->unit->run($test, $expected_result, $test_name, $notes);
+	}
+
+	/**
+	 * Funcion para testear el alta de una persona existente
+	 * @return void
+	 */
+	public function consulta_equipo_puntual_test()
+	{
+		#$test = $this->Equipo_model->consulta(4294967299, NULL);
+		$test = $this->Equipo_model->consulta($this->id_equipo, NULL);
+		$expected_result = 'is_object';
+		$test_name = 'consulta_equipo_puntual_test';
+		$notes = var_export($test, true);
+		$this->unit->run($test, $expected_result, $test_name, $notes);
+	}
+
+	/**
+	 * Funcion para testear el alta de una persona existente
+	 * @return void
+	 */
+	public function consulta_equipo_inexistente_test()
+	{
+		#$test = $this->Equipo_model->consulta(4294967299, NULL);
+		$test = $this->Equipo_model->consulta(0, NULL);
+		$expected_result = 'is_array';
+		$test_name = 'consulta_equipo_inexistente_test';
+		$notes = var_export($test, true);
+		$this->unit->run($test, $expected_result, $test_name, $notes);
+	}
+	
+	/**
+	 * Funcion para testear la consulta del equipo cargado en el test Alta equipo
+	 * @return void
+	 */
+	public function consulta_equipo_liga_test()
+	{
+		#$test = $this->Equipo_model->consulta(4294967299, NULL);
+		$test = $this->Equipo_model->consulta(NULL, 1);
+		$expected_result = 'is_array';
+		$test_name = 'consulta_equipo_por_liga_test';
+		$notes = var_export($test, true);
+		$this->unit->run($test, $expected_result, $test_name, $notes);
+	}
+	
+	/**
+	 * Funcion para testear la consulta del equipo cargado en el test Alta equipo
+	 * @return void
+	 */
+	public function baja_test($id_equipo)
+	{
+		$equipo = new stdClass();
+		
+		$equipo->id_equipo=$id_equipo;
+		/*
+		$equipo->id_liga=1;
+		$equipo->nombre='aldosivi';
+		$equipo->id_estadio=1;
+		$equipo->id_usuario=1;
+		$equipo->fecha_creacion='20160101';
+		$equipo->imagen='C:\Bitnami\wampstack-5.6.19-0\apps\yaftekun\db\imagenes\aldosivi.png';
+		*/
+		
+		$test = $this->Equipo_model->baja($equipo);
+		$expected_result['resultado']='OK';
+		$test_name = 'baja_parametro_test';
+		$notes = var_export($test, true);
+		if (!$this->iniciado){
+			echo '<>eliminando el equipo: '.$id_equipo.'</p>';
+			echo $this->unit->run($test, $expected_result, $test_name, $notes);
+		}
+		else{
+			$this->unit->run($test, $expected_result, $test_name, $notes);
+		}
+		#echo $this->unit->report();
+	}
+
+	
+	
+
+	
+	
+}
---
+++ app/application/models/Equipo_model.php
@@ -0,0 +1,152 @@
+<?php
+class Equipo_model extends CI_Model {
+
+	/**
+	 * Variables para los stored procedures usados por el modelo
+	 * @var string
+	 */
+	private $sp_consulta 	= 'call equipo_consulta(?, ?)';
+	private $sp_alta 		= 'call equipo_alta(?, ?, ?, ?, ?)';
+	private $sp_editar 		= 'call equipo_editar(?, ?, ?, ?, ?)';
+	private $sp_baja 		= 'call equipo_baja(?)';
+
+	/**
+	 * Variables para los atributos del modelo
+	 * @var bigint
+	*/
+	public $id_equipo;
+	/**
+	 * Variables para los atributos del modelo
+	 * @var bigint
+	 */
+	public $id_liga;
+	/**
+	 * Variables para los atributos del modelo
+	 * @var varchar(100)
+	 */
+	public $nombre;
+	/**
+	 * Variables para los atributos del modelo
+	 * @var bigint
+	 */
+	public $id_estadio;
+	/**
+	 * Variables para los atributos del modelo
+	 * @var bigint
+	 */
+	public $id_usuario;
+	/**
+	 * Variables para los atributos del modelo
+	 * @var datetime
+	*/
+	public $fecha_creacion;
+	/**
+	 * Variables para los atributos del modelo
+	 * @var mediumblob
+	*/
+	public $imagen;
+	
+	public function __construct()
+	{
+		$this->load->database();
+	}
+
+	/**
+	 * Consulta de equipos
+	 *
+	 * Consulta de torneos por ID o por Anio
+	 * @param 		bigint 	$id_equipo
+	 * @param 		bigint 	$id_liga
+	 * @return 		mixed 		object|array Si se consulta para clave primaria retorna un objeto.  Caso contrario retorna un array.
+	 */
+	#public function consulta($id_equipo=NULL, $id_liga = NULL)
+	public function consulta($id_equipo, $id_liga)
+	{
+		$query = $this->db->query($this->sp_consulta, array('id_equipo_IN' => $id_equipo, 'id_liga_IN' => $id_liga ));
+
+		if($id_equipo)
+		{
+			if ($query->num_rows() > 0) {
+				$row=$query->row_array();
+				$this->id_equipo	=$row["id_equipo"];
+				$this->id_liga		=$row["id_liga"];
+				$this->nombre		=$row["nombre"];
+				$this->id_estadio	=$row["id_estadio"];
+				$this->id_usuario	=$row["id_usuario"];
+				$this->fecha_creacion	=$row["fecha_creacion"];
+				$this->imagen		=$row["imagen"];
+
+			}
+			return $this;
+		}
+		else
+		{
+			return $query->result_array();
+		}
+
+	}
+
+	/**
+	 * Alta de equipo
+	 * @param		object	$equipo
+	 * @return 		array Devuelve un array con la la clave 'resultado', OK en caso de alta exitosa y sino ERROR
+	 */
+	public function alta($equipo)
+	{
+		$query = $this->db->query($this->sp_alta,
+				array(
+						'id_liga_IN' 		=> $equipo->id_liga,
+						'nombre_IN' 		=> $equipo->nombre,
+						'id_estadio_IN' 	=> $equipo->id_estadio,
+						'id_usuario_IN' 	=> $equipo->id_usuario,
+						#'fecha_creacion_IN' => $equipo->fecha_creacion,
+						'imagen_IN'			=>$equipo->imagen
+				));
+
+		if( $query )
+		{
+			$resultado['resultado']='OK';
+			$resultado['id']=$query->row_array()["id"];
+		}
+		else
+			$resultado['resultado']='ERROR';
+
+			return $resultado;
+	}
+
+	/**
+	 * Edicion de persona
+	 * @param		object	$equipo
+	 * @return 		array Devuelve un array con la la clave 'resultado', OK en caso de alta exitosa y sino ERROR
+	 */
+	public function editar($equipo)
+	{
+		if($this->db->query($this->sp_editar,
+				array(
+						'id_equipo_IN'	=> $equipo->id_equipo,
+						'id_liga_IN'	=> $equipo->id_liga,
+						'nombre_IN'	 	=> $equipo->nombre,
+						'id_estadio_IN'	=> $equipo->id_estadio,
+						'id_usuario_IN'	=> $equipo->id_usuario,
+						'imagen_IN'	 	=> $equipo->imagen
+				))
+				)
+			$resultado['resultado']='OK';
+			else
+				$resultado['resultado']='ERROR';
+				return $resultado;
+	}
+
+	/**
+	 * Baja de torneo
+	 * @return 		array Devuelve un array con la la clave 'resultado', OK en caso de alta exitosa y sino ERROR
+	 */
+	public function baja($equipo)
+	{
+		if($query = $this->db->query($this->sp_baja, array('id_equipo_IN' => $equipo->id_equipo)))
+			$resultado['resultado']='OK';
+			else
+				$resultado['resultado']='ERROR';
+				return $resultado;
+	}
+}
