2190959a3e0bd307a1556a919bcb5debaff0a37a
elnegrocok <carloskoz@gmail.com>
2016-05-13 18:27:56 -0300

    VERSION MARCO IS BACK

--- app/application/views/index.php
+++ app/application/views/index.php
@@ -1,21 +1,3 @@
-    <div class="jumbotron">
-        <div class="container text-center">
-            <h1>
-                <img class="img" src="<?php echo base_url("images/logotorneo.png"); ?>" alt="logo" width="34" height="42" style="margin: 10px">
-                TORNEO SENIOR OFICIAL</h1>
-        </div>
-    </div>
-
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Navegación</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
 
 
     <div class="container">
@@ -23,12 +5,9 @@
         <div class="row col-lg-4 col-md-4 ">
 
             <div class="panel panel-default">
-                <div class="panel-heading">
-                    TORNEOS CREADOS
-                </div>
+                <div class="panel-heading"><?php echo lang('form_label_torneos');?></div>
                 <div class="panel-body">
-                    <p>SASA SARASA </p>
-                    <p>COPA DE INTER CLUBS</p>
+   					<?php echo $torneos;?>
                 </div>
                 <!-- /.panel-body -->
             </div>
@@ -48,7 +27,7 @@
                                 </span>
                                 <span class="col-xs-9 text-right">
                                     <span class="huge">8</span><br>
-                                    <span>TORNEOS</span>
+                                    <span><?php echo lang('form_label_torneos');?></span>
                                 </span>
                             </a>
                         </div>
@@ -67,7 +46,7 @@
                                 </span>
                                 <span class="col-xs-9 text-right">
                                     <span class="huge">8</span><br>
-                                    <span>EQUIPOS</span>
+                                    <span><?php echo lang('form_label_equipos');?></span>
                                 </span>
                             </a>
                         </div>
@@ -87,7 +66,7 @@
                                 </span>
                                 <span class="col-xs-9 text-right">
                                     <span class="huge">265</span><br>
-                                    <span>JUAGADORES</span>
+                                    <span><?php echo lang('form_label_jugadores');?></span>
                                 </span>
                             </a>
                         </div>
@@ -106,7 +85,7 @@
                                 </span>
                                 <span class="col-xs-9 text-right">
                                     <span class="huge">22</span><br>
-                                    <span>DIRECTORES TÉCNICOS</span>
+                                    <span><?php echo lang('form_label_directores_tecnicos');?></span>
                                 </span>
                             </a>
                         </div>
@@ -124,7 +103,7 @@
                                 </span>
                                 <span class="col-xs-9 text-right">
                                     <span class="huge">22</span><br>
-                                    <span>ÁRBITROS</span>
+                                    <span><?php echo lang('form_label_arbitros');?></span>
                                 </span>
                             </a>
                         </div>
@@ -142,7 +121,7 @@
                                 </span>
                                 <span class="col-xs-9 text-right">
                                     <span class="huge">22</span><br>
-                                    <span>DELEGADOS</span>
+                                    <span><?php echo lang('form_label_delegados');?></span>
                                 </span>
                             </a>
                         </div>
