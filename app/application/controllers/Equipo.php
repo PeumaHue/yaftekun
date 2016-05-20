40e853bb57b222e2b8f07e48dfa35792afa9b83b
marianocat <mariano.andolfatto@gmail.com>
2016-05-20 15:25:26 -0300

    Helper HYaftekun y avance en equipos (la vista principal de equipos se puede usar como generica....

--- app/application/controllers/Equipo.php
+++ app/application/controllers/Equipo.php
@@ -12,48 +12,49 @@ class Equipo extends CI_Controller {
 		parent::__construct();
 		$this->load->model('Equipo_model');
 		$this->load->helper('url');
+		$this->load->helper('HYaftekun');
 	}
 	
 	public function index() {
-		#$data['items'] = $this->Equipo_model->consulta(NULL,NULL);
-				
-		$this->_renderizar_datos(NULL, NULL);
-		
+		$this->variables['html_datos_ppal'] =_renderizar_datos_link('equipo/editar', 'id_equipo','nombre',$this->Equipo_model->consulta(NULL,NULL));
 		$this->_setear_variables();
 				
 		$this->load->view('templates/header');
-		$this->load->view('templates/principal',$this->variables);
+		$this->load->view('equipos/principal',$this->variables);
 		$this->load->view('equipos/datos_equipo');
 		$this->load->view('templates/footer');
 	}
 	
 	/**
-	 * Funcion para generar una lista de equipos que se pasara en una variable a la pagina
-	 * se carga en la variable $variables['datos_ppal'] 
-	 * @var string
+	 * Funcion para generar un nuevo equipo
 	 */
-	private function _renderizar_datos($idequipo, $idliga){
-		$datos_ppal='<ul class="list-inline small interlineado-X1">';
-		foreach (($this->Equipo_model->consulta($idequipo, $idliga)) as $item):
-			$datos_ppal=$datos_ppal.'<li class="col-xs-12 col-sm-3">'.$item['nombre'].'</li>';
-		endforeach;
-		$datos_ppal=$datos_ppal.'</ul>';
+	public function alta(){
+		$this->_renderizar_datos(NULL, NULL);
+		$this->_setear_variables();
 		
-		$this->variables['datos_ppal'] = $datos_ppal;
+		$this->load->view('templates/header');
+		$this->load->view('equipos/principal',$this->variables);
+		$this->load->view('equipos/datos_equipo');
+		$this->load->view('templates/footer');
 	}
 	
 	
 	private function _setear_variables($titulo=NULL, $mensaje=NULL, $accion=NULL, $cancelar=NULL, $volver=NULL)
 	{
+		$this->variables['accion'] = $accion;
+		
 		$this->variables['form_id'] = '"equipos"';
 		$this->variables['titulo_label'] = 'html_equipo_titulo_default';
-		$this->variables['titulo_formato_panel'] = 'panel panel-equipos';
-		$this->variables['grilla_ppal_label'] = 'form_label_equipos';
+		$this->variables['funcionaltabusqueda'] = 'equipo/alta';
+		$this->variables['titulo_formato_panel'] = 'panel-equipos';
+		$this->variables['grilla_ppal_label'] = 'form_label_equipo';
 		
 		/*
+		 * 
+		 * 
 		$this->variables['titulo'] = $titulo;
 		$this->variables['mensaje'] = $mensaje;
-		$this->variables['accion'] = $accion;
+		
 		$this->variables['cancelar'] = $cancelar;
 		$this->variables['volver'] = $volver;
 		*/
