1cd26408df78fed8203fd865634e39b22aab5b00
Marco Cupo <cuttlas88@yahoo.com.ar>
2016-07-01 17:57:51 -0300

    Inicio de ABM arbitro

--- app/js/valida_arbitro.js
+++ app/js/valida_arbitro.js
@@ -1,35 +1,56 @@
 ﻿$(document).ready(function () {
-    $('#domicilio_body').on('shown.bs.collapse', function () {
-        $('#flecha_domicilio').removeClass().addClass("fa fa-chevron-up");
-    });
+	setear_botones();
+	validar_arbitro();
+	$("#imagen").filestyle('buttonText', 'Agregar');
+    $("#imagen").on('change', function() {
+        //Get count of selected files
+        var countFiles = $(this)[0].files.length;
+        var imgPath = $(this)[0].value;
+        var extn = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
+        var image_holder = $("#thumbnail_fotoparticipante_caption");
+        image_holder.empty();
+        if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
+          if (typeof(FileReader) != "undefined") {
+            //loop for each file selected for uploaded.
+            for (var i = 0; i < countFiles; i++) 
+            {
+              var reader = new FileReader();
+              reader.onload = function(e) {
+                $("<img />", {
+                  "src": e.target.result,
+                  "class": "img-thumbnail"
+                }).appendTo(image_holder);
+                var elem = document.getElementById("foto");
+                if (elem != null)
+              	  {
+              	     elem.parentElement.removeChild(elem);
+                          	  
+              	  }
+              }
+              image_holder.show();
+              reader.readAsDataURL($(this)[0].files[i]);
+            }
+          } else {
+            alert("This browser does not support FileReader.");
+          }
+        } else {
+          alert("Pls select only images");
+        }
+      });
+});
 
-    $('#domicilio_body').on('hidden.bs.collapse', function () {
-        $('#flecha_domicilio').removeClass().addClass("fa fa-chevron-down");
-    });
+function setear_botones()
+{
+	originalLocation = window.location.href;
+	if(originalLocation.indexOf("jugador/alta") >= 0)//Si esta en el alta no se muestra el boton eliminar
+	{
+		document.getElementById('btn_eliminar').style.visibility = "hidden";
+	}	
+}
 
-    $('#arbitros_body').on('shown.bs.collapse', function () {
-        $('#flecha_domicilio').removeClass().addClass("fa fa-chevron-up");
-    });
-
-    $('#arbitros_body').on('hidden.bs.collapse', function () {
-        $('#flecha_domicilio').removeClass().addClass("fa fa-chevron-down");
-    });
-     
-    
-    
-    $('#txt_NroDoc').keypress(function() {return validarNumeroControl(event);});
-    $('#txt_FechaNacimiento').keypress(function() { return validarNumeroControl(event);});
-    $('#txt_FechaNacimiento').keypress(function() {return maskFecha(this);});
-    $('#txt_Numero').keypress(function() {return validarNumeroControl(event);});
-    $('#txt_Telefono').keypress(function() {return validarNumeroControl(event);});
-    $('#txt_Celulalar').keypress(function() {return validarNumeroControl(event);});
-    $('#txt_Radio').keypress(function() {return validarNumeroControl(event);});
-
-
-
-
-    $('#arbitro').bootstrapValidator({
-        message: 'Este valor no es valido',
+function validar_arbitro()
+{
+    $('#participante').bootstrapValidator({
         feedbackIcons: {
             valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-remove',
@@ -39,118 +60,114 @@
             apellido: {
                 validators: {
                     notEmpty: {
-                        message: 'El nombre de equipo es obligatorio'
-                    }
+                        message: 'Hey!! no te olvides de ingresar el apellido del jugador'
+                    	},
+                    	regexp: {
+                            regexp: /^[a-zA-Z ]*$/,
+                            message: 'Ups! Solo se admiten letras!'
+                        }
                 }
             },
             nombre: {
                 validators: {
                     notEmpty: {
-                        message: 'El nombre de equipo es obligatorio'
-                    }
-                }
-            },
-            tipo_documento: {
-                validators: {
-                    notEmpty: {
-                        message: 'Debe seleccionar un tipo documento'
-                    }
-                }
-            },
-            numero_documento: {
-                validators: {
-                    notEmpty: {
-                        message: 'Debe ingresar el número de documento'
+                        message: 'Ups!! no te olvides de ingresar el nombre del jugador'
+                    },
+                	regexp: {
+                        regexp: /^[a-zA-Z ]*$/,
+                        message: 'Ups! Solo se admiten letras!'
                     }
                 }
             },            
-            calle: {
+            id_tipo_doc: {
                 validators: {
                     notEmpty: {
-                        message: 'El nombre de la calle es obligatoria'
+                        message: 'Ojo!! selecciona un tipo de documento'
+                    }
+                }
+            },
+            nro_doc: {
+                validators: {
+                    notEmpty: {
+                        message: 'Hey!! ingresa un numero de documento'
+                    	},
+            		digits: {
+            			message: 'Ojo!! solo se admiten numeros'
+            			}
+                	},
+            }, 
+            fecha_nacimiento: {
+                validators: {
+                    regexp: {
+                        regexp: /^(0?[1-9]|[12][0-9]|3[01])[\/](0?[1-9]|1[012])[/\\/](19|20)\d{2}$|^\s*$/,
+                        message: 'Ups! El formato de la fecha no es el correcto! deberia ser DD/MM/AAAA'
+                    }
+                }
+            },
+            nacionalidad:{
+                validators: {
+                	regexp: {
+                        regexp: /^[a-zA-Z ]*$/,
+                        message: 'Ups! Solo se admiten letras!'
+                    }
+                	},
+            }, 
+            conyuge_nombre:{
+                validators: {
+                	regexp: {
+                        regexp: /^[a-zA-Z ]*$/,
+                        message: 'Ups! Solo se admiten letras!'
+                    }
+                	},
+            }, 
+            fecha_apto_medico: {
+                validators: {
+                    regexp: {
+                		regexp: /^(0?[1-9]|[12][0-9]|3[01])[\/](0?[1-9]|1[012])[/\\/](19|20)\d{2}$|^\s*$/,
+                        message: 'Ups! El formato de la fecha no es el correcto! deberia ser DD/MM/AAAA'
                     }
                 }
             },
             numero: {
                 validators: {
-                    notEmpty: {
-                        message: 'La altura es obligatoria'
-                    },
-                    regexp: {
-                        regexp: /^[0-9]*$/,
-                        message: "El valor debe ser numérico"
-                    }
-
-                }
-            },
-            fecha_nacimiento: {
-                validators: {
-                    regexp: {
-                        regexp: /^(0?[1-9]|[12][0-9]|3[01])[\/](0?[1-9]|1[012])[/\\/](19|20)\d{2}$/,
-                        message: 'Código postal no valido'
+                	regexp: {
+                		regexp: /^[0-9]*$|^\s*$/,
+                        message: 'Ojo!! solo se admiten numeros'
                     }
                 }
             },
-
-            codigopostal: {
+            telefono:{
                 validators: {
-                    regexp: {
-                        regexp: /(^[0-9]{4}$)|(^[A-Za-z]{1}[0-9]{4}[A-Za-z]{3}$)/,
-                        message: 'Código postal no valido'
+                	regexp: {
+                		regexp: /^[0-9]*$|^\s*$/,
+                        message: 'Ojo!! solo se admiten numeros'
                     }
-                }
+                	},
+            }, 
+            telefono_celular:{
+                validators: {
+                	regexp: {
+                		regexp: /^[0-9]*$|^\s*$/,
+                        message: 'Ojo!! solo se admiten numeros'
+                    }
+                	},
+            }, 
+            telefono_radio:{
+                validators: {
+                	regexp: {
+                		regexp: /^[0-9]*$|^\s*$/,
+                        message: 'Ojo!! solo se admiten numeros'
+                    }
+                	},
+            }, 
+            email:{
+            	 validators: {
+            		 regexp: {
+                 		regexp: /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$|^\s*$/,
+                         message: 'Hey!! la direccion de email no es correcta!'
+                     }
+            	 	 },
             },
-            provincia: {
-                validators: {
-                    notEmpty: {
-                        message: 'Debe seleccionar una provincia'
-                    }
-                }
-            },
-            localidad: {
-                validators: {
-                    notEmpty: {
-                        message: 'Debe seleccionar una localidad'
-                    }
-                }
-            },
-            telefono: {
-                validators: {
-                    digits: {
-                        message: 'solo se admiten números'
-                    },
-                    notEmpty: {
-                        message: 'debe ingresar un teléfono'
-                    }
-                }
-            },
-            celular: {
-                validators: {
-                    digits: {
-                        message: 'solo se admiten números'
-                    },
-                    notEmpty: {
-                        message: 'debe ingresar un teléfono'
-                    }
-                }
-            },
-            radio: {
-                validators: {
-                    digits: {
-                        message: 'solo se admiten números'
-                    },
-                    notEmpty: {
-                        message: 'debe ingresar un teléfono'
-                    }
-                }
-            },
-            mail: {
-                validators: {
-                    emailAddress: {
-                        messge: 'mail incorrecto'
-                    }
-                }
-            }              
-        }
-    });
-});
+           }
+       });
+   }
